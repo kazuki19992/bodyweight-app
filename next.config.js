6d2afc8acec5234925269b622a0db47ec60b895f
kazuki19992 <kushida98@gmail.com>
2023-06-28 23:23:26 +0900

    :rocket: Pwa

--- next.config.js
+++ next.config.js
@@ -1,12 +1,19 @@
-/** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa")({
   dest: "public",
-  register: true,
-  skipWaiting: true,
+  disable:
+    process.env.NODE_ENV === "development"
+  // disable is help to disable PWA in deployment mode
 });
 
-const nextConfig = withPWA({
+/** @type {import('next').NextConfig} */
+module.exports = withPWA({
+  swcMinify: true,
   reactStrictMode: true,
-})
-
-module.exports = nextConfig
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
+  // write additional configuration here.
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,19 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <Head>
+        <meta name="application-name" content="BodyWeight" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="BodyWeight" />
+
+        <meta name="theme-color" content="#ea580c" />
+
+        <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+        <link rel="apple-touch-icon" sizes="192x192" href="/icon-192x192.png" />
+        <link rel="apple-touch-icon" sizes="256x256" href="/icon-256x256.png" />
+        <link rel="apple-touch-icon" sizes="512x512" href="/icon-512x512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon.png"></link>
-        <meta name="theme-color" content="#fff" />
+        <link rel="shortcut icon" href="/favicon.png" />
       </Head>
       <body className="h-screen w-screen bg-gradient-to-br from-orange-100 text-black/95 dark:from-orange-700 dark:to-orange-950 dark:text-white/95">
         {children}
